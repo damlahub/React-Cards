c7075cbf7d2968a8385c619811881549a8ca8763
DamlaOksel <damlaoksel@hotmail.com>
2023-08-08 18:21:54 +0300

    props mantığı öğrenildi

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import  Course  from './Course';
 
 function App() {
   return (
     <div className="App">
+      <Course title="Angular" description="Deserunt dolor cillum excepteur Lorem laboris. Ut eiusmod ad est aliqua occaecat dolore consequat incididunt eiusmod aliquip consequat dolor. Dolor voluptate est velit id elit non est cillum do aliqua pariatur irure. Sit ullamco sint nisi eiusmod ullamco dolor eu. Et incididunt tempor non ex irure dolore culpa magna ullamco non nulla."/>
+      <Course title="Bootstrap 5" description="Deserunt dolor cillum excepteur Lorem laboris. Ut eiusmod ad est aliqua occaecat dolore consequat incididunt eiusmod aliquip consequat dolor. Dolor voluptate est velit id elit non est cillum do aliqua pariatur irure. Sit ullamco sint nisi eiusmod ullamco dolor eu. Et incididunt tempor non ex irure dolore culpa magna ullamco non nulla."/>
+      <Course title="Komple Web" description="Deserunt dolor cillum excepteur Lorem laboris. Ut eiusmod ad est aliqua occaecat dolore consequat incididunt eiusmod aliquip consequat dolor. Dolor voluptate est velit id elit non est cillum do aliqua pariatur irure. Sit ullamco sint nisi eiusmod ullamco dolor eu. Et incididunt tempor non ex irure dolore culpa magna ullamco non nulla."/>
+      <Course title="Backend" description="Deserunt dolor cillum excepteur Lorem laboris. Ut eiusmod ad est aliqua occaecat dolore consequat incididunt eiusmod aliquip consequat dolor. Dolor voluptate est velit id elit non est cillum do aliqua pariatur irure. Sit ullamco sint nisi eiusmod ullamco dolor eu. Et incididunt tempor non ex irure dolore culpa magna ullamco non nulla."/>
     </div>
   );
 }
-
 export default App;
---
+++ cards/src/Course.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Course({title,description}){
+    return(
+        <div>
+            <div>{title}</div>
+            <div>{description}</div>
+        </div>
+    );
+}
+export default Course;
