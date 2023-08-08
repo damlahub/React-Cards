d13bebdbbf7514d639d285211adaf37bdd476e4c
DamlaOksel <damlaoksel@hotmail.com>
2023-08-08 19:32:13 +0300

    bulma frameworkü ile css eklendi

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,3 +1,4 @@
+import "bulma/css/bulma.css";
 import "./App.css";
 import Course from "./Course";
 import Angular from "./images/angular.jpg";
@@ -8,26 +9,46 @@ import KompleWeb from "./images/kompleweb.jpg";
 function App() {
   return (
     <div className="App">
-      <Course
-        image={Angular}
-        title="Angular"
-        description="Deserunt dolor cillum excepteur Lorem laboris. Ut eiusmod ad est aliqua occaecat dolore consequat incididunt eiusmod aliquip consequat dolor. Dolor voluptate est velit id elit non est cillum do aliqua pariatur irure. Sit ullamco sint nisi eiusmod ullamco dolor eu. Et incididunt tempor non ex irure dolore culpa magna ullamco non nulla."
-      />
-      <Course
-        image={Bootstrap}
-        title="Bootstrap 5"
-        description="Deserunt dolor cillum excepteur Lorem laboris. Ut eiusmod ad est aliqua occaecat dolore consequat incididunt eiusmod aliquip consequat dolor. Dolor voluptate est velit id elit non est cillum do aliqua pariatur irure. Sit ullamco sint nisi eiusmod ullamco dolor eu. Et incididunt tempor non ex irure dolore culpa magna ullamco non nulla."
-      />
-      <Course
-        image={CSharp}
-        title="Komple Web"
-        description="Deserunt dolor cillum excepteur Lorem laboris. Ut eiusmod ad est aliqua occaecat dolore consequat incididunt eiusmod aliquip consequat dolor. Dolor voluptate est velit id elit non est cillum do aliqua pariatur irure. Sit ullamco sint nisi eiusmod ullamco dolor eu. Et incididunt tempor non ex irure dolore culpa magna ullamco non nulla."
-      />
-      <Course
-        image={KompleWeb}
-        title="Backend"
-        description="Deserunt dolor cillum excepteur Lorem laboris. Ut eiusmod ad est aliqua occaecat dolore consequat incididunt eiusmod aliquip consequat dolor. Dolor voluptate est velit id elit non est cillum do aliqua pariatur irure. Sit ullamco sint nisi eiusmod ullamco dolor eu. Et incididunt tempor non ex irure dolore culpa magna ullamco non nulla."
-      />
+      <section class="hero is-danger">
+        <div class="hero-body">
+          <p class="title">KURSLAR</p>
+          <p class="subtitle">Kendini Her Daim Geliştir!</p>
+        </div>
+      </section>
+      <div className="container">
+      <section className="section">
+        <div className="columns">
+          <div className="column">
+            <Course
+              image={Angular}
+              title="Angular"
+              description="Deserunt dolor cillum excepteur Lorem laboris. Ut eiusmod ad est aliqua occaecat dolore consequat incididunt eiusmod aliquip consequat dolor. Dolor voluptate est velit id elit non est cillum do aliqua pariatur irure. Sit ullamco sint nisi eiusmod ullamco dolor eu. Et incididunt tempor non ex irure dolore culpa magna ullamco non nulla."
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Bootstrap}
+              title="Bootstrap 5"
+              description="Deserunt dolor cillum excepteur Lorem laboris. Ut eiusmod ad est aliqua occaecat dolore consequat incididunt eiusmod aliquip consequat dolor. Dolor voluptate est velit id elit non est cillum do aliqua pariatur irure. Sit ullamco sint nisi eiusmod ullamco dolor eu. Et incididunt tempor non ex irure dolore culpa magna ullamco non nulla."
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={CSharp}
+              title="Komple Web"
+              description="Deserunt dolor cillum excepteur Lorem laboris. Ut eiusmod ad est aliqua occaecat dolore consequat incididunt eiusmod aliquip consequat dolor. Dolor voluptate est velit id elit non est cillum do aliqua pariatur irure. Sit ullamco sint nisi eiusmod ullamco dolor eu. Et incididunt tempor non ex irure dolore culpa magna ullamco non nulla."
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={KompleWeb}
+              title="Backend"
+              description="Deserunt dolor cillum excepteur Lorem laboris. Ut eiusmod ad est aliqua occaecat dolore consequat incididunt eiusmod aliquip consequat dolor. Dolor voluptate est velit id elit non est cillum do aliqua pariatur irure. Sit ullamco sint nisi eiusmod ullamco dolor eu. Et incididunt tempor non ex irure dolore culpa magna ullamco non nulla."
+            />
+          </div>
+        </div>
+        </section>
+      </div>
     </div>
   );
 }
--- cards/src/Course.js
+++ cards/src/Course.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from "react";
 
-function Course({title,description,image}){
-    return(
-        <div>
-            <img src={image} />
-            <div>{title}</div>
-            <div>{description}</div>
+function Course({ title, description, image }) {
+  return (
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src={image} alt="Placeholder image" />
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{title}</p>
+          </div>
         </div>
-    );
+        <div class="content">
+          {description}
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Course;
